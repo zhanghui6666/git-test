8c04c72e2eaf6e077cde833296c9ddec3d133761
zhanghui6666 <1301550292@qq.com>
2022-05-08 15:50:01 +0800

    first

---
+++ src/main/java/com/zh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zh.git;
+
+/**
+ * @Author administered
+ * @Date 2022/5/8 15:46
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
