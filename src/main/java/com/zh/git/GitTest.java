a0b83c84350c4c5341620389274b4d889f925f7c
zhanghui6666 <1301550292@qq.com>
2022-05-08 15:58:09 +0800

    hot-fix commit

--- src/main/java/com/zh/git/GitTest.java
+++ src/main/java/com/zh/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello gitHub");
         System.out.println("hello gitHub3");
+        System.out.println("hello gitHub4");
     }
 }
